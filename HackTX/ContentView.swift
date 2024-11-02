5c398f5c588906bde3e3bdebf9cad384fc570bed
stardestro <25290886+stardestro@users.noreply.github.com>
2024-11-02 18:43:05 -0500

    first motion of box

--- HackTX/ContentView.swift
+++ HackTX/ContentView.swift
@@ -14,23 +14,49 @@ struct ContentView : View {
         RealityView { content in
 
             // Create a cube model
-            let model = Entity()
+//            let model = Entity()
+            let model = ModelEntity()
             let mesh = MeshResource.generateBox(size: 0.1, cornerRadius: 0.005)
             let material = SimpleMaterial(color: .gray, roughness: 0.15, isMetallic: true)
             model.components.set(ModelComponent(mesh: mesh, materials: [material]))
-            model.position = [0, 0.05, 0]
-
+//            model.
+            model.physicsBody = PhysicsBodyComponent(massProperties: .default, material: .generate(), mode: .dynamic)
+            model.generateCollisionShapes(recursive: true)
+            model.physicsBody?.isAffectedByGravity = false
+            model.position = [0, 0.1, -0.3]
+//            let shape = ShapeResource.generateBox(size: [0.1,0.1,0.1])
+//            model.components.set(CollisionComponent(shapes: [shape]))
+//            var physicspart = PhysicsBodyComponent(massProperties: .default, material: .generate(), mode: .dynamic)
+//            physicspart.isAffectedByGravity = false
+//            model.components.set(physicspart)
+//            model.physicsMotion = PhysicsMotionComponent()
+            
+//            model.components.set(PhysicsBodyComponent())
+//            model.physicsBody?.isAffectedByGravity = true
+            
+//            model.physicsBody = physicspart
             // Create horizontal plane anchor for the content
-//            let anchor = AnchorEntity(.plane(.horizontal, classification: .any, minimumBounds: SIMD2<Float>(0.2, 0.2)))
+            let anchor = AnchorEntity(.plane(.horizontal, classification: .any, minimumBounds: SIMD2<Float>(0.2, 0.2)))
 //            let anchor = AnchorEntity(.image(group: "AR Resources" , name: "apriltag.jpg"))
 //            anchor.addChild(model)
-            let anchor = AnchorEntity(.)
-            //            anchor.addChild(model)
+//            let anchor = AnchorEntity(.camera)
+            
+            let impulseStrength = SIMD3<Float>(0, 0, 1)
+            let impulseDirection = SIMD3<Float>(0, 0, 1)
+//            model.applyLinearImpulse(impulseStrength * impulseDirection, relativeTo: model)
+            model.applyLinearImpulse(SIMD3<Float>(0, 0, 0.2), relativeTo: model)
+            anchor.addChild(model)
+            
+//            model.components.physic
 
             // Add the horizontal plane anchor to the scene
             content.add(anchor)
+            
+//            model.transform.translation = [0,0,-0.5]
 
             content.camera = .spatialTracking
+            
+//            model.modelDebugOptions = ModelDebugOptionsComponent(visualizationMode: .normal)
 
         }
         .edgesIgnoringSafeArea(.all)
---
+++ HackTX/Powersystem.swift
@@ -0,0 +1,58 @@
+//
+//  Powersystem.swift
+//  HackTX
+//
+//  Created by Hussain Alkatheri on 11/2/24.
+//
+
+//@available(iOS 15.0, *)
+//class ChangeMeshAndColorSystem: System {
+//    static let query = EntityQuery(where:
+//                                  .has(ModelComponent.self) &&
+//                                  .has(PhysicsBodyComponent.self))
+//    
+//    required init(scene: Scene) {
+//        print("RealityKit's first System is activated")
+//    }
+//    func update(context: SceneUpdateContext) {
+//        context.scene.performQuery(Self.query).forEach { entity in
+//            guard let entity = entity as? ModelEntity
+//            else { return }
+//            entity.model?.mesh = .generateSphere(radius: 0.1)
+//            var material = PhysicallyBasedMaterial()
+//            material.baseColor.tint = .systemOrange
+//            entity.model?.materials = [material]
+//        }
+//    }
+//}
+
+import RealityKit
+
+struct alien: Component {
+    // Optionally, put any needed state here.
+}
+
+
+class Powersystem: System {
+
+
+    // Define a query to return all entities with a MyComponent.
+    private static let query = EntityQuery(where: .has(alien.self))
+
+
+    // Initializer is required. Use an empty implementation if there's no setup needed.
+    required init(scene: Scene) { }
+
+
+    // Iterate through all entities containing a MyComponent.
+    func update(context: SceneUpdateContext) {
+        for entity in context.entities(
+            matching: Self.query,
+            updatingSystemWhen: .rendering
+        ) {
+            // Make per-update changes to each entity here.
+//            entity.force
+            
+        }
+    }
+}
