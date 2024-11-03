a6948482689284dd3d6e6d52ddabbf168205317b
stardestro <25290886+stardestro@users.noreply.github.com>
2024-11-03 02:43:41 -0600

    3x3 waves and collision

--- HackTX/ContentView.swift
+++ HackTX/ContentView.swift
@@ -10,6 +10,9 @@ import RealityKit
 import Combine
 import ARKit
 
+
+
+
 struct ContentView : View {
     
     @State var collisionSubscriptions = [RealityFoundation.EventSubscription]()
@@ -18,40 +21,61 @@ struct ContentView : View {
     @State var cameraNode: AnchorEntity = AnchorEntity()
     @State var bullet: ModelEntity = ModelEntity()
     
+    
+    
+    let timer = Timer.publish(every: 5, on: .main, in: .common).autoconnect()
+    
+    func spawn(position: SIMD3<Float>) {
+        let box = ModelEntity()
+        let mesh = MeshResource.generateBox(size: 0.1, cornerRadius: 0.005)
+        let material = SimpleMaterial(color: .gray, roughness: 0.15, isMetallic: true)
+        box.components.set(ModelComponent(mesh: mesh, materials: [material]))
+        box.physicsBody = PhysicsBodyComponent(massProperties: .default, material: .generate(), mode: .dynamic)
+        box.generateCollisionShapes(recursive: true)
+        box.physicsBody?.isAffectedByGravity = false
+        box.position = position
+//            let anchor = AnchorEntity(.image(group: "AR Resources" , name: "tag"))
+        box.applyLinearImpulse(SIMD3<Float>(0, 0, 0.2), relativeTo: nil)
+        alien.registerComponent()
+        box.components.set(alien())
+        
+        rootNode.addChild(box)
+    }
+    
     var body: some View {
         VStack{
             RealityView { content in
-                
+                lazar.registerComponent()
 //                rootNode = AnchorEntity(.image(group: "AR Resources" , name: "tag"))
                 rootNode = AnchorEntity(.plane(.horizontal, classification: .any, minimumBounds:     [0.1, 0.1]))
                 cameraNode = AnchorEntity(.camera)
                 
-                let deathbox = ModelEntity()
-                let deathmesh = MeshResource.generateBox(size: 0.3, cornerRadius: 0.005)
-                let deathmaterial = SimpleMaterial(color: .gray, roughness: 0.15, isMetallic: true)
-                deathbox.components.set(ModelComponent(mesh: deathmesh, materials: [deathmaterial]))
-//                deathbox.physicsBody = PhysicsBodyComponent(massProperties: .default, material: .generate(), mode: .static)
-                deathbox.collision = CollisionComponent(shapes: [ShapeResource.generateBox(width: 0.1, height: 0.1, depth: 0.1)], mode: .trigger)
-                deathbox.generateCollisionShapes(recursive: true)
-                deathbox.physicsBody?.isAffectedByGravity = false
-                deathbox.position = [0, 0.5, -1]
+//                let deathbox = ModelEntity()
+//                let deathmesh = MeshResource.generateBox(size: 0.3, cornerRadius: 0.005)
+//                let deathmaterial = SimpleMaterial(color: .gray, roughness: 0.15, isMetallic: true)
+//                deathbox.components.set(ModelComponent(mesh: deathmesh, materials: [deathmaterial]))
+////                deathbox.physicsBody = PhysicsBodyComponent(massProperties: .default, material: .generate(), mode: .static)
+//                deathbox.collision = CollisionComponent(shapes: [ShapeResource.generateBox(width: 0.1, height: 0.1, depth: 0.1)], mode: .trigger)
+//                deathbox.generateCollisionShapes(recursive: true)
+//                deathbox.physicsBody?.isAffectedByGravity = false
+//                deathbox.position = [0, 0.5, -3]
                 
     //            deathbox.applyLinearImpulse(SIMD3<Float>(0, 0, 0.1), relativeTo: nil)
                 
-                rootNode.addChild(deathbox)
-
-                let box = ModelEntity()
-                let mesh = MeshResource.generateBox(size: 0.1, cornerRadius: 0.005)
-                let material = SimpleMaterial(color: .gray, roughness: 0.15, isMetallic: true)
-                box.components.set(ModelComponent(mesh: mesh, materials: [material]))
-                box.physicsBody = PhysicsBodyComponent(massProperties: .default, material: .generate(), mode: .dynamic)
-                box.generateCollisionShapes(recursive: true)
-                box.physicsBody?.isAffectedByGravity = false
-                box.position = [0, 0.5, 0]
-    //            let anchor = AnchorEntity(.image(group: "AR Resources" , name: "tag"))
-                box.applyLinearImpulse(SIMD3<Float>(0, 0, -0.2), relativeTo: nil)
-                
-                rootNode.addChild(box)
+//                rootNode.addChild(deathbox)
+//
+//                let box = ModelEntity()
+//                let mesh = MeshResource.generateBox(size: 0.1, cornerRadius: 0.005)
+//                let material = SimpleMaterial(color: .gray, roughness: 0.15, isMetallic: true)
+//                box.components.set(ModelComponent(mesh: mesh, materials: [material]))
+//                box.physicsBody = PhysicsBodyComponent(massProperties: .default, material: .generate(), mode: .dynamic)
+//                box.generateCollisionShapes(recursive: true)
+//                box.physicsBody?.isAffectedByGravity = false
+//                box.position = [0, 0.5, 0]
+//    //            let anchor = AnchorEntity(.image(group: "AR Resources" , name: "tag"))
+//                box.applyLinearImpulse(SIMD3<Float>(0, 0, -0.2), relativeTo: nil)
+//                
+//                rootNode.addChild(box)
                 
                 
                 
@@ -66,7 +90,11 @@ struct ContentView : View {
 //                rootNode.addChild(floor)
                 
                 collisionSubscriptions.append(content.subscribe(to: CollisionEvents.Began.self, { event in
-                    box.model?.materials = [SimpleMaterial(color: .blue, isMetallic: true)]
+//                    box.model?.materials = [SimpleMaterial(color: .blue, isMetallic: true)]
+                    if(event.entityA.components.has(alien.self) && event.entityB.components.has(lazar.self)){
+                        rootNode.removeChild(event.entityA)
+                        rootNode.removeChild(event.entityB)
+                    }
                 }))
                 // Add the horizontal plane anchor to the scene
                 content.add(rootNode)
@@ -76,22 +104,22 @@ struct ContentView : View {
     //            model.transform.translation = [0,0,-0.5]
                 
                 collisionSubscriptions.append(content.subscribe(to: CollisionEvents.Ended.self, { event in
-                    box.model?.materials = [SimpleMaterial(color: .red, isMetallic: true)]
-                    if(deathbox != event.entityA){
-                        rootNode.removeChild(event.entityA)
-                    }
-                    if(deathbox != event.entityB){
-                        rootNode.removeChild(event.entityB)
-                    }
+//                    box.model?.materials = [SimpleMaterial(color: .red, isMetallic: true)]
+//                    if(deathbox != event.entityA){
+//                        rootNode.removeChild(event.entityA)
+//                    }
+//                    if(deathbox != event.entityB){
+//                        rootNode.removeChild(event.entityB)
+//                    }
+                    
+//                    if(event.entityA.components.has(PhysicsBodyComponent.self)){
+//                        
+//                    }
                 }))
-
+                
                 content.camera = .spatialTracking
                 
-
-    //            model.modelDebugOptions = ModelDebugOptionsComponent(visualizationMode: .normal)
-                
             }
-//            .gesture(DragGesture().targetedToAnyEntity())
             Button {
                 let box = ModelEntity()
                 let mesh = MeshResource.generateBox(size: 0.1, cornerRadius: 0.005)
@@ -109,13 +137,21 @@ struct ContentView : View {
                 box.orientation = cameraNode.orientation(relativeTo: rootNode)
                 // TODO do matrix math to fix impulse
                 box.applyLinearImpulse(SIMD3<Float>(0, 0, -1), relativeTo: nil)
-                
+                box.components.set(lazar())
                 rootNode.addChild(box)
                 print("SHOT")
             } label: {
                 Text("Shoot")
             }
-
+            .onReceive(timer) { input in
+//                [0, 0.5, -1]
+                for i in 1...3{
+                    for j in 1...3{
+                        spawn(position: [(Float(j)*0.2)-0.5, ((Float(i)*0.2)), -2])
+                    }
+                }
+                
+            }
         }
         
 //        .edgesIgnoringSafeArea(.all)
--- HackTX/coalmines.swift
+++ HackTX/coalmines.swift
@@ -9,11 +9,60 @@ import Foundation
 import RealityKit
 import Combine
 
-class Coalmines {
+struct coalspawner: Component {
+    // Optionally, put any needed state here.
+    var previousDate: Date?
+}
+
+
+class coalSpawnersystem: System {
+
+
+    // Define a query to return all entities with a MyComponent.
+    private static let query = EntityQuery(where: .has(coalspawner.self))
+
+
+    // Initializer is required. Use an empty implementation if there's no setup needed.
+    required init(scene: Scene) { }
+
+
+    // Iterate through all entities containing a MyComponent.
+    func update(context: SceneUpdateContext) {
+        for entity in context.entities(
+            matching: Self.query,
+            updatingSystemWhen: .rendering
+        ) {
+            // Make per-update changes to each entity here.
+//            if(entity.components.previousDate < Date()){
+//
+//            }
+            
+        }
+    }
+}
+
+
+class spawner: NSObject
+{
+    private var anchor: AnchorEntity!
     
-    var collisionSubscriptions: [AnyCancellable] = []
+    func spawnloadtimer()
+    {
+        let timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(spawnload), userInfo: nil, repeats: true)
+    }
     
-    init() {
+    @objc func spawnload(){
+        let box = ModelEntity()
+        let mesh = MeshResource.generateBox(size: 0.1, cornerRadius: 0.005)
+        let material = SimpleMaterial(color: .gray, roughness: 0.15, isMetallic: true)
+        box.components.set(ModelComponent(mesh: mesh, materials: [material]))
+        box.physicsBody = PhysicsBodyComponent(massProperties: .default, material: .generate(), mode: .dynamic)
+        box.generateCollisionShapes(recursive: true)
+        box.physicsBody?.isAffectedByGravity = false
+        box.position = [0, 0.5, 0]
+//            let anchor = AnchorEntity(.image(group: "AR Resources" , name: "tag"))
+        box.applyLinearImpulse(SIMD3<Float>(0, 0, -0.2), relativeTo: nil)
         
+        anchor.addChild(box)
     }
 }
